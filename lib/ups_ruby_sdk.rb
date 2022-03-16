28d4232befdc2983bd8626af03b332ac8528fa02
Xavi Ablaza <25378966+xaviablaza@users.noreply.github.com>
2022-03-16 08:28:38 +0800

    Initial commit

---
+++ lib/ups_ruby_sdk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ups_ruby_sdk/version"
+
+module UpsRubySdk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ups_ruby_sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UpsRubySdk do
+  it "has a version number" do
+    expect(UpsRubySdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
